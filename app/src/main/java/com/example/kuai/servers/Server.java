e6e729f5701411742ebc7eaeda5aa497e8d078a0
wangyongyue <18601271036@163.com>
2020-08-11 22:37:01 +0800

    m

--- app/src/main/java/com/example/kuai/servers/Server.java
+++ app/src/main/java/com/example/kuai/servers/Server.java
@@ -1,16 +1,25 @@
 package com.example.kuai.servers;
 
+import okhttp3.*;
+
+import java.io.IOException;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
 public class Server {
     public List<Object> list = new ArrayList<>();
+    public String error;
     public Server.OnDataListener listener;
     public String url(){return "";}
-    public HashMap body(){return new HashMap();}
+    public FormBody body(){
+            FormBody body = new FormBody.Builder()
+//            .add("a","b")
+            .build();
+        return body;
+    }
     public List<Object> getList() {return list;}
     public void loadTableData(Object reponse){}
+    public void loadError(String error){}
     public void tableIndex(int index){}
     public void reloadData(Server.OnDataListener l){ listener = l; };
     public void networkRequest(){
@@ -18,8 +27,32 @@ public class Server {
             loadTableData(null);
             listener.reload();
             return;
+        }else {
+
+            OkHttpClient client = new OkHttpClient();
+            Request request = new Request.Builder()
+                    .url(url())
+                    .post(body())
+                    .build();
+            Call call = client.newCall(request);
+            call.enqueue(new Callback() {
+                @Override
+                public void onFailure(Call call, IOException e) {
+                    loadError("网络错误");
+                    listener.reload();
+                }
+                @Override
+                public void onResponse(Call call, Response response) throws IOException {
+                    if(response.isSuccessful()){
+                        String result = response.body().string();
+                        //处理UI需要切换到UI线程处理
+                        loadTableData(response.body());
+                        listener.reload();
+                    }
+                }
+            });
         }
-    };
+    }
 
     public interface OnDataListener {
         void reload();
